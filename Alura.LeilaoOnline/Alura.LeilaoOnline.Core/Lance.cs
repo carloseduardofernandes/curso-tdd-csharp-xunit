fc96fc94ec9b4a0d5e427dc09d3539b912db441e
Carlos Eduardo Fernandes <carlos.eduardo.f@live.com>
2021-04-21 14:25:14 -0300

    adicionado solution, pastas, e projeto lib core

---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Core/Lance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Alura.LeilaoOnline.Core
+{
+    public class Lance
+    {
+        public Interessada Cliente { get; }
+        public double Valor { get; }
+
+        public Lance(Interessada cliente, double valor)
+        {
+            Cliente = cliente;
+            Valor = valor;
+        }
+    }
+}
