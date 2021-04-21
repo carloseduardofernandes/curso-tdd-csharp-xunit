e4926f6856c0f7f8750fff486360c528195dc784
Carlos Eduardo Fernandes <carlos.eduardo.f@live.com>
2021-04-21 18:41:28 -0300

    testes em console app

---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,80 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+
+namespace Alura.LeilaoOnline.ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            LeilaoComVariosLances();
+            LeilaoComApenasUmLance();
+        }
+
+        //tratamento manual, já existem bibliotecas que fazem esse trabalho
+
+        private static void LeilaoComApenasUmLance()
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 1200;
+            var valorObtido = leilao.Ganhador.Valor;
+            Verifica(valorEsperado, valorObtido);
+        }
+
+        private static void LeilaoComVariosLances()
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+
+            leilao.RecebeLance(maria, 990);
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert (esta falho, pois precisa de humano ver e verificar se está ok os dados)
+            /*Console.WriteLine($"Leilão finalizado, Ganhador: " +
+                $"{leilao.Ganhador.Cliente.Nome} " +
+                $" Valor: { leilao.Ganhador.Valor}");*/
+
+            //Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Verifica(valorEsperado, valorObtido);
+        }
+
+        private static void Verifica(int valorEsperado, double valorObtido)
+        {
+            var cor = Console.ForegroundColor;
+
+            if (valorEsperado == valorObtido)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("TESTE OK");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"TESTE FALHOU! Esperado: {valorEsperado}," +
+                    $" Valor Obtido: {valorObtido}");
+            }
+
+            Console.ForegroundColor = cor;
+        }
+    }
+}
